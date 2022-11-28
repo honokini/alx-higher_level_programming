00157cddca8e36520eb419af6fdff2552e0e8b3d
honokini <hkini48@gmail.com>
2022-11-28 15:31:40 -0800

    10-check_cycle.c

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,9 +1,9 @@
 #ifndef LISTS_H                                                                                                                             
 #define LISTS_H                                                                                                                             
-
                                                                                                                                             
 #include <stdlib.h>                                                                                                                         
-
+                                                                                                                                            
+                                                                                                                                            
 typedef struct listint_s
 {
 	int n;
