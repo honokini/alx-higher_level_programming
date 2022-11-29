eb964933c15f479d229feb41dd243e636c10ab75
honokini <hkini48@gmail.com>
2022-11-29 15:53:20 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-# KINI Bie Honore
+# Author - KINI Bie Honore
 
 for digit1 in range(0, 10):
     digit2 in range(digit + 1, 10):
         if digit1 == 8 and digit2 == 9:
             print("{}{}".format(digit1, digit2))
         else:
-            print("{}{}".format(digit1; digit2), end=", ")
+            print("{}{}".format(digit1, digit2), end=", ")
