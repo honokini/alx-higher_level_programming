0743a877a4e625c7995f04ec2fff874a5a6094cb
honokini <hkini48@gmail.com>
2022-11-29 17:05:56 -0800

    10-add.py

--- 0x01-python-if_else_loops_functions/100-print_tebalpha.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3                                                              # Author - KINI Bie Honore                                                                                                                  
-i = 0                                                                                                                                       
-for c in range(ord('z'), ord('a') - 1, - 1):                                    
-    print("{}.format(chr(c - i)), end="")                                                            
-            i = 32  if i == 0 else 0
