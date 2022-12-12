733c0f628a5e26d7ab8174ef1cee08665a681377
honokini <hkini48@gmail.com>
2022-12-12 12:46:41 -0800

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,6 +3,7 @@
 /**
  * reverse_listint - reverses a linked list
  * @head: pointer to the first node in the list
+ *
  * Return: pointer to the first node in the new list
  */
 void reverse_listint(listint_t **head)
@@ -21,6 +22,7 @@ void reverse_listint(listint_t **head)
 
 	*head = prev;
 }
+
 /**
  * is_palindrome - checks if a linked list is a palindrome
  * @head: double pointer to the linked list
