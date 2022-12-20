874252f6a0242688251becd3af67ff0315bb18d3
honokini <hkini48@gmail.com>
2022-12-20 12:14:32 -0800

    2-square.p, 2-main.py

--- 0x06-python-classes/2-main.py
+++ 0x06-python-classes/2-main.py
@@ -14,21 +14,21 @@ try:
 except Exception as e:
         print(e)
 
-        try:
-                print(my_square_1.__size)
-        except Exception as e:
-                print(e)
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
 
-                try:
-                        my_square_3 = Square("3")
-                            print(type(my_square_3))
-                                print(my_square_3.__dict__)
-                except Exception as e:
-                        print(e)
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+except Exception as e:
+    print(e)
 
-                        try:
-                                my_square_4 = Square(-89)
-                                    print(type(my_square_4))
-                                        print(my_square_4.__dict__)
-                        except Exception as e:
-                                print(e)
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
+except Exception as e:
+    print(e)
