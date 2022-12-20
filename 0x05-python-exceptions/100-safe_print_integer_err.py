598fc2d5834ed0f627092ce94a508154f1e95649
honokini <hkini48@gmail.com>
2022-12-20 09:19:03 -0800

    100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -19,5 +19,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return (True)
     except (TypeError, ValueError):
-        print("Exeception: {}".format(sys.exc_info()[1], file=sys.stderr)
+        print("Exception: {}".format(sys.exc_info()[1], file=sys.stderr)
                 return (False)
