2875bdbeec1fa079f92e6dd06f746ac29d96fcdc
honokini <hkini48@gmail.com>
2023-01-09 15:51:15 -0800

    5-rectangle.py

--- 0x08-python-more_classes/5-main.py
+++ 0x08-python-more_classes/5-main.py
@@ -10,8 +10,3 @@ try:
     print(my_rectangle)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
-
-guillaume@ubuntu:~/0x08$ ./5-main.py
-Area: 8 - Perimeter: 12
-Bye rectangle...
-[NameError] name 'my_rectangle' is not defined
