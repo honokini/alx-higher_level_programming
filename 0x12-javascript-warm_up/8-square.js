a072d89c076fbfde53b496a6dbc1c2f9f58ed467
honokini <hkini48@gmail.com>
2023-03-14 02:02:50 +0000

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,3 +1,4 @@
+ble file 17 lines (15 sloc) 266 Bytes
 #!/usr/bin/node
 const x = process.argv[2];
 
@@ -7,3 +8,11 @@ if (!parseInt(x)) {
   for (let i = 0; i < x; i++) {
     let y = 0;
     let myVar = '';
+
+    while (y < x) {
+      myVar = myVar + 'X';
+      y++;
+    }
+    console.log(myVar);
+  }
+}
