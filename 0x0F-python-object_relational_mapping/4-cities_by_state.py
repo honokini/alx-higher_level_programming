0b3d4092f61d7ac1cd47cfea986b72615eab15f8
honokini <hkini48@gmail.com>
2023-03-17 03:41:35 -0700

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,62 +1,27 @@
--- Create states table in hbtn_0e_4_usa with some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_4_usa;
+#!/usr/bin/python3
+"""
+This script lists all cities from
+the database `hbtn_0e_4_usa`.
+"""
 
-USE hbtn_0e_4_usa;
+import MySQLdb as db
+from sys import argv
 
-CREATE TABLE IF NOT EXISTS states (
-	id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id)
-);
+if __name__ == '__main__':
+    """
+    Access the database and get the cities
+    from the database.
+    """
 
-INSERT INTO
-	states (name)
-VALUES
-	("California"),
-	("Arizona"),
-	("Texas"),
-	("New York"),
-	("Nevada");
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
 
-CREATE TABLE IF NOT EXISTS cities (
-	id INT NOT NULL AUTO_INCREMENT,
-	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY(state_id) REFERENCES states(id)
-);
+    with db_connect.cursor() as db_cursor:
+        db_cursor.execute("SELECT cities.id, cities.name, states.name \
+                                FROM cities JOIN states ON cities.state_id \
+                                = states.id ORDER BY cities.id ASC")
+        rows_selected = db_cursor.fetchall()
 
-INSERT INTO
-	cities (state_id, name)
-VALUES
-	(1, "San Francisco"),
-	(1, "San Jose"),
-	(1, "Los Angeles"),
-	(1, "Fremont"),
-	(1, "Livermore");
-
-INSERT INTO
-	cities (state_id, name)
-VALUES
-	(2, "Page"),
-	(2, "Phoenix");
-
-INSERT INTO
-	cities (state_id, name)
-VALUES
-	(3, "Dallas"),
-	(3, "Houston"),
-	(3, "Austin");
-
-INSERT INTO
-	cities (state_id, name)
-VALUES
-	(4, "New York");
-
-INSERT INTO
-	cities (state_id, name)
-VALUES
-	(5, "Las Vegas"),
-	(5, "Reno"),
-	(5, "Henderson"),
-	(5, "Carson City");
+    if rows_selected is not None:
+        for row in rows_selected:
+            print(row)
